1aba0e2326e2677bb004bbb77422d7bbb642d3b6
Luis Dalmolin <luis.nh@gmail.com>
2019-10-21 20:52:28 -0300

    Added database test

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = factory(User::class)->create();
+        $this->assertTrue($user->exists);
+    }
+}
